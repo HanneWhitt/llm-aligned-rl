44193352ef1e945ce1850fcbe7ca85ac94bea9c1
HanneWhitt <hannes.whittingham@gmail.com>
2024-09-13 18:24:40 +0100

    some testing of homegrid env

---
+++ check_homegrid.py
@@ -0,0 +1,74 @@
+from stable_baselines3.common.env_checker import check_env
+from matplotlib import pyplot as plt
+
+import gym
+
+import homegrid
+
+# from homegrid.window import Window
+# import matplotlib.pyplot as plt
+# from tokenizers import Tokenizer
+
+
+env = gym.make("homegrid-cat")
+obs, info = env.reset()
+
+print(obs)
+input("\n.reset() obs output\n\n")
+print(info)
+input("\n.reset() info output\n\n")
+
+
+
+print('STEP')
+obs, reward, terminated, truncated, info = env.step(0)
+print('step over')
+
+print(obs)
+input("\n.step() obs output\n\n")
+print(reward)
+input("\n.step() reward output\n\n")
+print(terminated)
+input("\n.step() terminated output\n\n")
+print(truncated)
+input("\n.step() truncated output\n\n")
+print(info)
+input("\n.step() info output\n\n")
+
+
+
+
+print(env.action_space)
+print('env.action_space')
+input()
+
+
+
+print(env.observation_space)
+print('env.observation_space')
+input()
+
+
+
+print(env.metadata)
+print('env.metadata')
+input()
+
+
+print(env.render_mode)
+print('env.render_mode')
+input()
+
+print(env.reward_range)
+print('env.reward_range')
+input()
+
+print(env.spec)
+print('env.spec')
+
+
+rend = env.render()
+print(rend)
+input('\n.render() output \n\n')
+plt.imshow(rend)
+plt.show()
---
+++ examine_cartpole.py
@@ -0,0 +1,11 @@
+import gymnasium as gym
+
+env = gym.make("CartPole-v1")
+
+obs, info = env.reset()
+
+
+print(obs)
+print(type(obs))
+
+
